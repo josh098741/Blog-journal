c65b5ace55d7b3787c20a32f5a6372df49ef77d4
josh098741 <morongemokaya@gmail.com>
2025-10-18 15:33:13 +0300

    Additional commits on addition of the header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,2 +1,21 @@
+import { Link } from 'react-router-dom'
 
+function Header(){
+    return(
+        <div className="bg-gradient-to-b from-slate-900 flex flex-col justify-center items-center h-32">
+            <div>
+                <h1 className="text-xl text-white">Blog Journal</h1>
+            </div>
+            <div className="flex gap-5 relative">
+                <div className="border w-32 text-center">
+                    <Link to="/">Home</Link>
+                </div>
+                <div className="border w-32 text-center">
+                    <Link to="/create">Create Post</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
 
+export default Header
